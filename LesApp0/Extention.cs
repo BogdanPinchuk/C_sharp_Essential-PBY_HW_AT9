3bb19ddf60672827f8613afe4694e52d4fe73a57
Bogdan <40561032+BogdanPinchuk@users.noreply.github.com>
2019-07-24 00:12:37 +0300

    Добавьте файлы проекта.

---
+++ LesApp0/Extention.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp0
+{
+    /// <summary>
+    /// Клас для методів розширення
+    /// </summary>
+    static class Extention
+    {
+        /// <summary>
+        /// Метод розришення для масиву рядків
+        /// </summary>
+        /// <param name="array">масив рядків</param>
+        /// <param name="pred">делегет-предікат</param>
+        public static void ExtentionMethod(this string[] array, Predicate pred)
+        {
+            // правпор на те чи щось знайдено із вхідного масиву
+            bool find = false;
+
+            // перебираємо вхідний масив
+            for (int i = 0; i < array.Length; i++)
+            {
+                // перевіряємо чи рядок  відповідає умовам
+                if (pred(array[i]))
+                {
+                    // 
+                    Console.WriteLine("\t" + array[i]);
+                    find = true;
+                }
+            }
+
+            // якщо нічого не найдено, то виведем сповіщення
+            if (!find)
+            {
+                Console.WriteLine("\n\tНе знайдено жодного рядка який би відповідав умові.");
+            }
+        }
+
+    }
+}
---
+++ LesApp4/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp4
+{
+    /// <summary>
+    /// Арифметична операція
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    delegate double AO(double a, double b);
+
+    class Program
+    {
+        static void Main()
+        {
+            // Enable Unicode
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // набір допутимих опреацій
+            string arrayOper = "+-/*";
+
+            // Алгоритм Дейкстри
+            for (; ; )
+            {
+                // чистка консолі
+                Console.Clear();
+
+                #region Перше число
+                // введеня даних
+                Console.Write("Введіть число: a = ");
+
+                // зчитування числа
+                bool next = double.TryParse(Console.ReadLine()
+                    .Replace(".", ","), out double a);
+
+                // перевірка чи не було ввежено неправильно
+                if (!next)
+                {
+                    Console.WriteLine("\n\tПомилка введення даних.\n" +
+                        "\tСпробувати ще раз?");
+                    if (Console.ReadKey().Key == ConsoleKey.Enter)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                #endregion
+
+                #region Арифметична операція
+                // введеня даних
+                Console.Write("Введіть арифметичну опрецію: ");
+
+                // зчитування операції
+                string oper = Console.ReadLine();
+
+                // аналіз чи введений 1 символ і чи введена АО є в наборі
+                if (!(oper.Length == 1 &&
+                    arrayOper.Contains(oper)))
+                {
+                    Console.WriteLine("\n\tПомилка введення даних.\n" +
+                        "\tСпробувати ще раз?");
+                    if (Console.ReadKey().Key == ConsoleKey.Enter)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                #endregion
+
+                #region Друге число
+                // введеня даних
+                Console.Write("Введіть число: b = ");
+
+                // зчитування числа
+                next = double.TryParse(Console.ReadLine()
+                    .Replace(".", ","), out double b);
+
+                // перевірка чи не було ввежено неправильно
+                if (!next)
+                {
+                    Console.WriteLine("\n\tПомилка введення даних.\n" +
+                        "\tСпробувати ще раз?");
+                    if (Console.ReadKey().Key == ConsoleKey.Enter)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                #endregion
+
+                // створення делегата який виконуватиме АО
+                AO del = default;
+
+                // аналіз вибраної операції
+                switch (oper)
+                {
+                    case "+":   // лямда-вираз
+                        del = (x, y) => x + y;
+                        break;
+                    case "-":   // лямда-вираз
+                        del = (x, y) => x - y;
+                        break;
+                    case "*":   // лямда-вираз
+                        del = (x, y) => x * y;
+                        break;
+                    case "/":   // анонімний метод
+                        del = delegate (double x, double y)
+                        {
+                            if (a == 0 && b == 0)
+                            {
+                                return double.NaN;
+                            }
+                            else if (a > 0 && b == 0)
+                            {
+                                return double.PositiveInfinity;
+                            }
+                            else if (a < 0 && b == 0)
+                            {
+                                return double.NegativeInfinity;
+                            }
+                            else
+                            {
+                                return a / b;
+                            }
+                        };
+                        break;
+                }
+
+                // вивід результату
+                Console.WriteLine($"\n\tРезультат: {a} {oper} {b} = {del(a, b):N2}");
+
+                #region Повтор
+                Console.WriteLine("\n\tСпробувати ще раз?");
+                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                } 
+                #endregion
+            }
+        }
+
+        #region Арифметичні операції, які можна було б присвоїти делегату
+        /// <summary>
+        /// Сума чисел
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private static double Add(double a, double b) => a + b;
+        /// <summary>
+        /// Різниця чисел
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private static double Sub(double a, double b) => a - b;
+        /// <summary>
+        /// Добуток чисел
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private static double Mul(double a, double b) => a * b;
+        /// <summary>
+        /// Частка чисел
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private static double Div(double a, double b)
+        {
+            // перевірка на 0
+            double temp = default;
+
+            // врахування виключень
+            if (a == 0 && b == 0)
+            {
+                temp = double.NaN;
+            }
+            else if (a > 0 && b == 0)
+            {
+                temp = double.PositiveInfinity;
+            }
+            else if (a < 0 && b == 0)
+            {
+                temp = double.NegativeInfinity;
+            }
+            else
+            {
+                temp = a / b;
+            }
+
+            return temp;
+        } 
+        #endregion
+
+    }
+}
